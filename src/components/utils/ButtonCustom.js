2b7fc733fe2735c3073c964f427a35b3008864cb
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2021-12-12 17:32:14 -0300

    button shield composition

---
+++ src/components/utils/ButtonCustom.js
@@ -0,0 +1,22 @@
+import { Button, Link } from '@mui/material';
+
+const styles = {
+    root: {
+        padding: '1rem 2rem',
+        color: 'white',
+        mb: 16,
+    },
+};
+
+export const ButtonCustom = ({ children, align }) => {
+    return (
+        <Button sx={styles.root} variant='outlined'>
+            <Link
+                sx={{ textDecoration: 'none' }}
+                href='mailto:alejandro.barraza1997@gmail.com?Subject=Hello'
+            >
+                {children}
+            </Link>
+        </Button>
+    );
+};
