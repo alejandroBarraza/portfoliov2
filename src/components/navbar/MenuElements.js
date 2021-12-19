45406576d1811eeb7ee95142c0288f86de3ed7ed
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2021-12-19 01:20:21 -0300

    smooth scroll mobile view

--- src/components/navbar/MenuElements.js
+++ src/components/navbar/MenuElements.js
@@ -4,6 +4,8 @@ import { MenuItem, Typography } from '@mui/material';
 import { cyan } from '@mui/material/colors';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
+import { Link as LinkScroll } from 'react-scroll';
+
 const styles = {
     menuColor: {
         '& .MuiPaper-root': {
@@ -49,23 +51,57 @@ export const MenuElements = ({ handleClose, anchorEl }) => {
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
-                <MenuItem onClick={handleClose}>
-                    <CloseRoundedIcon sx={{ ml: 'auto' }} />
-                </MenuItem>
-
-                <MenuItem onClick={handleClose}>
-                    <Typography sx={{ pr: 1, color: cyan[500] }}>01.</Typography>
-                    <Typography> About</Typography>
-                </MenuItem>
-
-                <MenuItem onClick={handleClose}>
-                    <Typography sx={{ pr: 1, color: cyan[500] }}>02.</Typography>
-                    <Typography>Projects</Typography>
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                    <Typography sx={{ pr: 1, color: cyan[500] }}>03.</Typography>
-                    <Typography>Contact</Typography>
-                </MenuItem>
+                <LinkScroll
+                    activeClass='active'
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <CloseRoundedIcon sx={{ ml: 'auto' }} />
+                    </MenuItem>
+                </LinkScroll>
+                <LinkScroll
+                    activeClass='active'
+                    to='aboutMe'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Typography sx={{ pr: 1, color: cyan[500] }}>01.</Typography>
+                        <Typography> About</Typography>
+                    </MenuItem>
+                </LinkScroll>
+                <LinkScroll
+                    activeClass='active'
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Typography sx={{ pr: 1, color: cyan[500] }}>02.</Typography>
+                        <Typography>Projects</Typography>
+                    </MenuItem>
+                </LinkScroll>
+                <LinkScroll
+                    activeClass='active'
+                    to='contact'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={700}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Typography sx={{ pr: 1, color: cyan[500] }}>03.</Typography>
+                        <Typography>Contact</Typography>
+                    </MenuItem>
+                </LinkScroll>
             </Menu>
         </div>
     );
